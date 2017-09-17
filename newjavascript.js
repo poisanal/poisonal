1540bace6470d1e21e3fd3f8a9d09843280f5efc
poisanal <babuabende15@gmail.com>
2017-09-16 22:40:47 -0700

    Add files via upload

---
+++ newjavascript.js
@@ -0,0 +1,10 @@
+
+<html>
+<head>
+<body>
+<scripttype="text/javascript">
+document.write=write("Hello");
+</script>
+</head>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,59 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+<html>
+<jsp:useBean id="cb" scope="session" class="colors.ColorGameBean" />
+<jsp:setProperty name="cb" property="*" />
+<%
+cb.processRequest();
+%>
+
+<body bgcolor=<%= cb.getColor1() %>>
+<font size=6 color=<%= cb.getColor2() %>>
+<p>
+
+<% if (cb.getHint()==true) { %>
+
+<p> Hint #1: Vampires prey at night!
+<p><p> Hint #2: Nancy without the n.
+
+<% } %>
+
+<% if  (cb.getSuccess()==true) { %>
+
+<p> CONGRATULATIONS!!
+<% if  (cb.getHintTaken()==true) { %>
+
+<p>( although I know you cheated and peeked into the hints)
+
+<% } %>
+
+<% } %>
+
+<p> Total attempts so far: <%= cb.getAttempts() %>
+<p>
+
+<p>
+
+<form method=POST action=colrs.jsp>
+
+Color #1: <input type=text name= color1 size=16>
+
+<br>
+
+Color #2: <input type=text name= color2 size=16>
+
+<p>
+
+<input type=submit name=action value="Submit">
+<input type=submit name=action value="Hint">
+
+</form>
+
+</font>
+</body>
+</html>
